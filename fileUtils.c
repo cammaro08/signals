f2c618a9d90d5e10e62a71930108c51e57af8663
TS <talha.i.sheikh@gmail.com>
2020-03-28 21:56:10 +0000

    adding fileUtils to git

---
+++ fileUtils.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "fileUtils.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int out;
+int save_out;
+
+// int main(int argc, const char *argv[])
+// {
+ 
+//     initializeStatusFile();
+//     redirectStdout();
+//     //printf("HOWDY!");
+//     writeFileMetaData();
+//     flushOutputAndCloseFile();
+
+//     return 0;
+// }
+
+void initializeStatusFile() {
+    out = open("data/cout.log", O_RDWR|O_CREAT|O_APPEND, 0600);
+    if (-1 == out) { perror("opening cout.log"); exit(255); }
+}
+
+void redirectStdout() {
+   save_out = dup(fileno(stdout));
+
+    if (-1 == dup2(out, fileno(stdout))) { perror("cannot redirect stdout"); exit(255); }
+ 
+}
+
+void flushOutputAndCloseFile() {
+    fflush(stdout); close(out);
+    dup2(save_out, fileno(stdout));
+    close(save_out);
+
+}
+
+void writeFileMetaData() {
+    struct stat attrib;
+    stat("data/cout.log", &attrib);
+    printf("\n\n METADATA %s", &attrib.st_mtime);
+}
---
+++ fileUtils.h
@@ -0,0 +1,4 @@
+void initializeStatusFile(void);
+void redirectStdout(void);
+void flushOutputAndCloseFile(void);
+void writeFileMetaData(void);
